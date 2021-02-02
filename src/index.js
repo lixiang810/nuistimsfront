c65d7d0c83d8a2daa89c1490ee58393754c5c4c5
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2021-02-02 14:33:26 +0800

    rebuild

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Dashboard from './Dashboard'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const App = () => (
-    <Dashboard />
-)
-
-ReactDOM.render(<App />, document.getElementById('root'))
